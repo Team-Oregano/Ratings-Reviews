fc286879d75fc7ff3b75efca174f3e92c47eed69
Joseph_Sanfelippo <jsanfelippo82@gmail.com>
2022-04-27 18:01:35 -0500

    commit for deployment

--- __tests__/api/api_spec.js
+++ __tests__/api/api_spec.js
@@ -2,25 +2,21 @@ const frisby = require('frisby');
 
 
 it('should return success code', function() {
-  jest.setTimeout(60000);
   return frisby.get('http://localhost:3000/reviews')
     .expect('status', 200);
 });
 
 it('should return success code', function() {
-  jest.setTimeout(60000)
   return frisby.get('http://localhost:3000/reviewphotos')
   .expect('status', 200);
 })
 
 it('should return success code', function() {
-  jest.setTimeout(60000)
   return frisby.get('http://localhost:3000/characteristics')
   .expect('status', 200);
 })
 
   it('should return success code', function() {
-    jest.setTimeout(60000)
     return frisby.get('http://localhost:3000/characteristicsreviews')
-    .expect('status', 202);
-  })
+    .expect('status', 200);
+  });
